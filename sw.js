00fb393bed75cc4c97100e33caedab3bdb62a06a
Farhan <farhan.farooq@live.com>
2020-03-24 16:58:42 +0500

    pre-cached assets

--- sw.js
+++ sw.js
@@ -1,14 +1,37 @@
+const staticCacheName = "site-cache";
+const assets = [
+  "/",
+  "/index.html",
+  "/js/app.js",
+  "/js/ui.js",
+  "/js/materialize.min.js",
+  "/css/styles.css",
+  "/css/materialize.min.css",
+  "/img/dish.png",
+  "https://fonts.googleapis.com/icon?family=Material+Icons"
+];
+
 // Install service worker
 self.addEventListener("install", event => {
-  console.log("service worker has been installed", event);
+  // cache static resources
+  event.waitUntil(
+    caches
+      .open(staticCacheName)
+      .then(cache => {
+        // add resources to be cached
+        cache.addAll(assets);
+        console.log("cached static resources");
+      })
+      .catch(err => console.error(err))
+  );
 });
 
 // Listen for activate event
 self.addEventListener("activate", event => {
-  console.log("service worker has been activated", event);
+  // console.log("service worker has been activated", event);
 });
 
 // Fetch event
 self.addEventListener("fetch", event => {
-  console.log("fetch event", event);
+  // console.log("fetch event", event);
 });
