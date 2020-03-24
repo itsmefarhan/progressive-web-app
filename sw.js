70a2648e4690240b1a44d077a576dbcf94770589
Farhan <farhan.farooq@live.com>
2020-03-24 15:18:12 +0500

    service worker activate event

--- sw.js
+++ sw.js
@@ -1,4 +1,9 @@
-// install service worker
+// Install service worker
 self.addEventListener("install", event => {
   console.log("service worker has been installed", event);
+});
+
+// Listen for activate event
+self.addEventListener("activate", event => {
+  console.log("service worker has been activated", event);
 });
